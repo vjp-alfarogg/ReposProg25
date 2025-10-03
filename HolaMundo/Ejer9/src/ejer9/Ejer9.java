c937e205871dd4a1d2874b52ebc01a019fd80f79
1DAM - Alfaro García García <agarciag180@informatica.iesvalledeljerteplasencia.es>
2025-10-03 15:55:05 +0200

    Introduccion HolaMundo

---
+++ HolaMundo/Ejer9/src/ejer9/Ejer9.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejer9;
+
+/**
+ *
+ * @author Alfaro
+ */
+public class Ejer9{   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double pi = 3.141592, l, radio = 3.55;
+        
+        //calculo longitud de la circunferencia
+        l = pi * radio * 2;
+ 
+        System.out.println("La longitud es " + l);
+        System.out.println("PI:" + pi + " * " + "radio: " + radio + " = " + l);
+        
+    }
+    
+}
---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+import java.util.zip.CRC32;
+
+/**
+ * Mi primer programa
+ * 
+ * @version 1
+ * @author Alfaro
+ * @since 2025
+ */
+public class HolaMundo {
+
+    static int num1 = 10;
+    
+    final static float PI = 3.14F;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {       
+        
+        int num2 = 50;
+    
+        int resultado = num1 + num2;
+        
+        //Casting
+        short dato1;
+        int dato2 = 15;
+        
+        dato1 = (short) dato2;
+    }
+    
+    public void metodo2(){
+        
+    }
+}
